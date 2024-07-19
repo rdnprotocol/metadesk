51c92103f4608e70568b2aa56b38f868faeacdfb
Erdenetuuk <rdnprotocol@gmail.com>
2024-07-19 16:42:29 +0800

    landing update

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,31 +2,49 @@ import { BsFillArrowRightCircleFill } from "react-icons/bs";
 export const Landing = () => {
   return (
     <>
-      <div className="w-full h-[960px] bg-gradient-to-b from-[#dbfbf1] to-[#f8f9f9] ">
+      <div className="w-full h-screen bg-gradient-to-b from-[#dbfbf1] to-[#f8f9f9] ">
         <div className="container h-full mx-auto flex flex-col items-center pt-[150px]">
           <h1 className="text-[100px] text-center font-bold">
             ТАНЫ ОНЛАЙН ТУСЛАХ
           </h1>
-          <p>
+          <p className="text-lg">
             Бид танд болон танай компанид мэдээлэл технологийн бүх төрлийн
             үйлчилгээг санал болгож байна.{" "}
           </p>
           <div className=" grid  place-content-center gap-10 grid-cols-4 mt-[90px] h-fit w-fit">
-            <div className="bg-[#dbecff] h-[200px] w-[300px] rounded-md py-9 px-9 ">
-              <h1 className="text-3xl font-bold mb-6">ДУУДЛАГА ӨГӨХ</h1>
-              <BsFillArrowRightCircleFill size={40} />
+            <div className="bg-[#dbecff] h-[200px] w-[300px] rounded-md py-9 px-9  flex flex-col  ">
+              <h1 className="text-3xl font-bold ">ДУУДЛАГА ӨГӨХ</h1>
+              <p className="text-[12px] mb-3 ">үйлчилгээний захиалга үүсгэх</p>
+              <div className=" justify-self-end ">
+                {" "}
+                <BsFillArrowRightCircleFill size={35} />
+              </div>
             </div>
-            <div className="bg-[#dbecff] h-[200px] w-[300px] rounded-md py-9 px-9 ">
-              <h1 className="text-3xl font-bold mb-6">ДУУДЛАГА ӨГӨХ</h1>
-              <BsFillArrowRightCircleFill size={40} />
+            <div className="bg-[#dbecff] h-[200px] w-[300px] rounded-md py-9 px-9  flex flex-col  ">
+              <h1 className="text-3xl font-bold ">ПРОГРАМ ХАНГАМЖ</h1>
+              <p className="text-[12px] mb-3 ">
+                танд хэрэг болон онлайн програмууд
+              </p>
+              <div className=" justify-self-end ">
+                {" "}
+                <BsFillArrowRightCircleFill size={35} />
+              </div>
             </div>
-            <div className="bg-[#dbecff] h-[200px] w-[300px] rounded-md py-9 px-9 ">
-              <h1 className="text-3xl font-bold mb-6">ДУУДЛАГА ӨГӨХ</h1>
-              <BsFillArrowRightCircleFill size={40} />
+            <div className="bg-[#dbecff] h-[200px] w-[300px] rounded-md py-9 px-9 flex flex-col ">
+              <h1 className="text-3xl font-bold  ">МЭДЛЭГИЙН САН</h1>
+              <p className="text-[12px] mb-3 ">зөвлөгөө мэдээлэл</p>
+              <div className=" justify-self-end ">
+                {" "}
+                <BsFillArrowRightCircleFill size={35} />
+              </div>
             </div>
-            <div className="bg-[#dbecff] h-[200px] w-[300px] rounded-md py-9 px-9 ">
-              <h1 className="text-3xl font-bold mb-6">ДУУДЛАГА ӨГӨХ</h1>
-              <BsFillArrowRightCircleFill size={40} />
+            <div className="bg-[#dbecff] h-[200px] w-[300px] rounded-md py-9 px-9  flex flex-col ">
+              <h1 className="text-3xl font-bold ">НЭГДСЭН ФАЙЛЫН САН</h1>
+              <p className="text-[12px] mb-3">Танд хэрэгтэй</p>
+              <div className=" justify-self-end ">
+                {" "}
+                <BsFillArrowRightCircleFill size={35} />
+              </div>
             </div>
           </div>
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,38 +2,40 @@ import Link from "next/link";
 
 export const Navbar = () => {
   return (
-    <div className="flex justify-between mx-auto py-6 content-center items-center container">
-      <img
-        className="h-[60px] w-[100px] object-contain"
-        src="/White.png
+    <div className="relative">
+      <div className="flex justify-between mx-auto py-6 content-center items-center  fixed top-0 left-0 right-0 h-[60px] bg-white w-screen  ">
+        <div className=" flex justify-between mx-auto py-6 content-center items-center container">
+          <img
+            className="h-[60px] w-[100px] object-contain"
+            src="/White.png
 
       "
-      />
-      <div className="flex gap-8 text-lg ">
-        <div>Үнийн санал</div>
-        <Link href="">
-          <div>Холбоо барих</div>{" "}
-        </Link>
+          />
+          <div className="flex gap-8 text-lg ">
+            <h1 className="font-semibold">НҮҮР</h1>
 
-        <Link href="">
-          <div>Туслах</div>
-        </Link>
-      </div>
-      <div className="flex gap-3 ">
-        <div
-          className="bg-[#407ce8] text-sm text-center text-white w-[90px] h-fit py-1 px-1 rounded-md
+            <Link href="">
+              <h1 className="font-semibold">ҮЙЛЧИЛГЭЭ</h1>
+            </Link>
+          </div>
+          <div className="flex gap-3 ">
+            <div
+              className="bg-[#407ce8] text-sm text-center text-white w-[90px] h-fit py-1 px-1 rounded-md
 
 "
-        >
-          Нэвтрэх
-        </div>
-        {/* <div
+            >
+              Нэвтрэх
+            </div>
+          </div>
+
+          {/* <div
           className="bg-blue-400 text-sm text-white w-fit h-fit py-1 px-1 rounded-md
 
 "
         >
           Бүртгүүлэх
         </div> */}
+        </div>
       </div>
     </div>
   );
